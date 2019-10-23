db8aa40b0168af14c095171a373285a3eb65cb3f
hunghhdev <hunghh.dev@gmail.com>
2019-10-23 14:16:43 +0700

    namespace and static

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.viewlist, name='view_list'),
